0fcfda2ad4ec87a3075f5af67588a4f8f348671b
thairyo <sonthai1310.works@gmail.com>
2022-10-05 19:57:11 +0700

    Board Functions

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame
 import numpy as np
@@ -12,21 +13,93 @@ screen.fill(BG_COLOR)
 
 
 class Board:
+
     def __init__(self):
         self.squares = np.zeros((ROWS, COLS))
         # print(self.squares)
+        self.empty_sqr = self.squares  # [squares]
+        self.marked_sqrs = 0
+
+    def final_state(self):
+        '''
+            @return 0 if there is no win yet
+            @return 1 if player 1 wins
+            @return 2 if player 2 wins
+        '''
+        # vertical wins
+        for col in range(COLS):
+            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:
+                return self.squares[0][col]
+
+        # horizontal wins
+        for row in range(ROWS):
+            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
+                return self.squares[row][0]
+
+        # desc diagonal
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+            return self.squares[1][1]
+
+        # asc diagonal
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+            return self.squares[1][1]
+
+        # no win yet
+        return 0
 
     def mark_sqr(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_sqrs += 1
 
     def empty_sqr(self, row, col):
         return self.squares[row][col] == 0
 
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_sqr(row, col):
+                    empty_sqrs.append((row, col))
+        return empty_sqrs
+
+    def isfull(self):
+        return self.marked_sqrs == 9
+
+    def isempty(self):
+        return self.marked_sqrs == 0
+
+
+class AI:
+
+    def __init__(self, level=0, player=2):
+        self.level = level
+        self.player = player
+
+    def rnd(self, board):
+        empty_sprs = board.get_empty_sqrs()
+        idx = random.randrange(0, len(empty_sprs))
+
+        return empty_sprs[idx]  # (row, col)
+
+    def eval(self, main_board):
+        if self.level == 0:
+            # random choice
+            move = self.rnd(main_board)
+        else:
+            # minimax algorithm choice
+            pass
+
+        return move  # row, col
+
 
 class Game:
+
     def __init__(self):
         self.board = Board()
+        self.ai = AI()
         self.player = 1  # 1-cross # 2-circle
+        self.gamemode = 'pvp'  # pvp or ai
+        self.running = True
         self.show_lines()
 
     def show_lines(self):
@@ -71,6 +144,7 @@ def main():
     # object
     game = Game()
     board = game.board
+    ai = game.ai
 
     # main loop
     while True:
@@ -90,6 +164,13 @@ def main():
                     game.draw_fig(row, col)
                     game.next_turn()
                     # print(board.squares)
+
+        if game.gamemode == 'ai' and game.player == ai.player:
+            # update the screen
+            pygame.display.update()
+
+            # ai methods
+            row, col = ai.eval(board)
 
         pygame.display.update()
 
